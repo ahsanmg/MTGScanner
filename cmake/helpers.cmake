b575ef8ee3dc36f39dee47b7f865e3b20502d70d
Ahsan Ullah <hatrakhan@gmail.com>
2026-08-18 23:31:29 +0500

    Add copying dynamic libraries to the binary directory

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -12,3 +12,37 @@ function(mtgs_copy_assets target_name src dst)
         COMMENT "Syncing assets from ${src} to ${dst}/assets"
     )
 endfunction()
+
+function(mtgs_copy_dlls target_name src)
+    if (EXISTS ${src} AND NOT ${src} STREQUAL "")
+        if(WIN32)
+            set(LIB_EXTENSION "*.dll")
+        else()
+            set(LIB_EXTENSION "*.so*") # Maps to .so, .so.1, etc.
+        endif()
+
+        file(GLOB_RECURSE DLL_FILES LIST_DIRECTORIES FALSE "${src}/${LIB_EXTENSION}")
+        if(NOT DLL_FILES)
+            message(WARNING "No libraries found matching ${LIB_EXTENSION} in ${src}. Skipping target creation.")
+            return()
+        endif()
+
+        message(STATUS "Defining custom command for target ${target_name} copying directory ${src}")
+        foreach(file_path IN LISTS DLL_FILES)
+			message(STATUS "  - ${file_path}")
+		endforeach()
+
+        add_custom_target(${target_name}
+            COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}"
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                ${DLL_FILES}
+                "${CMAKE_BINARY_DIR}"
+
+            DEPENDS ${src}
+            COMMENT "Syncing DLL files for target ${target_name}"
+            VERBATIM
+        )
+    else()
+        message(FATAL_ERROR "Path not found: ${src}")
+    endif()
+endfunction()
